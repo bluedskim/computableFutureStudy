4fa87337303710fac70518b6091580bd43c5256b
Dennis <bluedskim@gmail.com>
2023-11-20 13:17:51 +0900

    add Logger

--- app/src/test/java/net/shed/lab/computable_future/ComputablefutureTest.java
+++ app/src/test/java/net/shed/lab/computable_future/ComputablefutureTest.java
@@ -3,13 +3,31 @@
  */
 package net.shed.lab.computable_future;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.logging.Logger;
 
 import org.junit.Test;
 
 public class ComputablefutureTest {
+	private static Logger logger = Logger.getLogger(ComputablefutureTest.class.getName());
+
 	@Test
-	public void appHasAGreeting() {
-		assertNotNull("app should have a greeting", "not null");
+	public void appHasAGreeting() throws InterruptedException, ExecutionException {
+		CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> "Hello");
+		logger.info(String.format("future1=%s", future1.get()));
+		CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> "Beautiful");
+		logger.info(String.format("future2=%s", future2.get()));
+		CompletableFuture<String> future3 = CompletableFuture.supplyAsync(() -> "World");
+		logger.info(String.format("future3=%s", future3.get()));
+
+		CompletableFuture<Void> combinedFuture = CompletableFuture.allOf(future1, future2, future3);
+		combinedFuture.get();
+
+		assertTrue(future1.isDone());
+		assertTrue(future2.isDone());
+		assertTrue(future3.isDone());
 	}
 }
