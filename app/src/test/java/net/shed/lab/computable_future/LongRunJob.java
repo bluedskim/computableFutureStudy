82a19d44f79e4c7d7e219ebbb6885bde1251f615
Dennis <bluedskim@gmail.com>
2023-11-20 15:25:34 +0900

    change return type of run() method

--- app/src/test/java/net/shed/lab/computable_future/LongRunJob.java
+++ app/src/test/java/net/shed/lab/computable_future/LongRunJob.java
@@ -3,10 +3,19 @@ package net.shed.lab.computable_future;
 import java.util.logging.Logger;
 
 public class LongRunJob {
+	int LOOP_CNT = 1000 * 100;
 	private static Logger logger = Logger.getLogger(LongRunJob.class.getName());
 
-	void run() {
-		logger.info("start");
-		logger.info("end");
+	String jobId;
+
+	public LongRunJob(String jobId) {
+		this.jobId = jobId;
+	}
+
+	String run() {
+		logger.info("start " + jobId + " / thread=" + Thread.currentThread().getName());
+		for(int i = 0 ; i < LOOP_CNT ; i++) {}
+		logger.info("end " + jobId);
+		return jobId + " done / thread=" + Thread.currentThread().getName();
 	}
 }
